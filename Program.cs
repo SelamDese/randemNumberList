389e01361aabd8b176efb433c25c366f7fdb7a41
selam <selam0315@gmail.com>
2019-06-06 15:46:58 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace randemNumbersList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
